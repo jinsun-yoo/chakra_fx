50d10c4d111ef4b6885f5a0e6cdf47e2a7d51d38
crmsndu <crmsndu@gmail.com>
2025-08-19 18:23:51 +0000

    Add Profiling

--- src/chakra_fx/passes/chakra_converter.py
+++ src/chakra_fx/passes/chakra_converter.py
@@ -1,4 +1,6 @@
+import csv
 import os
+from typing import Tuple
 
 import torch
 import torch._inductor.fx_utils as fx_utils
@@ -183,12 +185,12 @@ class ChakraConverter:
             fx_node.target(*args, **kwargs)
             return flop_counter_mode.get_total_flops(), True
 
-    def estimate_tensor_size(self, fx_node: fx.Node) -> int:
+    def estimate_tensor_size(self, fx_node: fx.Node) -> tuple:
         success, args, kwargs = fx_utils.get_fake_args_kwargs(fx_node)
         if not success:
             if os.environ["RANK"] == "0":
                 print(f"{node_debug_id_str(fx_node)} has estimated flopcount but no tensor_size: {fx_node.target._opname}")
-            return 0
+            return 0, (0, 0), (0, 0)
 
         # Assumption: The first two arguments are the input tensors.
         a = args[0].size()
@@ -197,11 +199,12 @@ class ChakraConverter:
         if fx_node.target._overloadpacket != torch.ops.aten.mm and fx_node.target._overloadpacket != torch.ops.aten._scaled_mm:
             a = args[1].size()
             b = args[2].size()
-        numbytes_per_element = 4
+        numbytes_per_element = 4  # FP32
         estimated_tensor_size = 2 * numbytes_per_element * (a[0] * a[1] + b[0] * b[1] + a[0] * b[1])
         if os.environ["RANK"] == "0":
             print(f"Estimated tensor size, a: {a[0]} {a[1]} b: {b[0]} {b[1]} result {estimated_tensor_size}")
-        return estimated_tensor_size
+
+        return estimated_tensor_size, a, b
 
     # Measure the duration of a compute operation by running it on actual GPU.
     # This is possible because we have 1) the operation and 2) the symbolic shape of the input tensors (i.e. FakeTensor).
@@ -263,25 +266,42 @@ class ChakraConverter:
             mean_duration_cuda_event = int(total_duration_cuda_event / num_iters)
         return mean_duration_cuda_event
 
+
+    def _profile_comp_node(self, fx_node: fx.Node) -> Tuple[int, int, torch.Size, torch.Size, int]:
+        estimated_flops, can_get_real_optarg = self.estimate_flop_count(fx_node)
+
+        estimated_tensor_size = 0
+        a_shape = torch.Size()
+        b_shape = torch.Size()
+        estimated_duration = 0
+
+        if can_get_real_optarg:
+            estimated_tensor_size, a_shape, b_shape = self.estimate_tensor_size(fx_node)
+            estimated_duration = self.measure_duration_microsecond(fx_node)
+
+        return (
+            estimated_flops,
+            estimated_tensor_size,
+            a_shape,
+            b_shape,
+            estimated_duration,
+        )
+
     # Create a Compute Chakra Node from an FX Node.
     # We need to add three attributes:
     # num_ops: number of flops, used for roofline analysis.
     # tensor_size: size of the input tensors, used for roofline analysis.
     # duration_micros: duration of the operation, used for replay.
     def create_comp_node(self, fx_node: fx.Node) -> ChakraNode:
-        node_name = fx_node.name
-        estimated_flops, can_get_real_optarg = self.estimate_flop_count(fx_node)
-        estimated_tensor_size = 0
-        estimated_duration = 0
-        if can_get_real_optarg:
-            estimated_tensor_size = self.estimate_tensor_size(fx_node)
-            estimated_duration = self.measure_duration_microsecond(fx_node)
+        flops, tensor_size, _, _, duration = self._profile_comp_node(fx_node)
 
+        node_name = fx_node.name
         chakra_node = self.create_chakra_node(node_name, COMP_NODE)
         chakra_node.attr.append(ChakraAttr(name="is_cpu_op", bool_val=False))
-        chakra_node.attr.append(ChakraAttr(name="num_ops", int64_val=estimated_flops))
-        chakra_node.attr.append(ChakraAttr(name="tensor_size", uint64_val=estimated_tensor_size))
-        chakra_node.duration_micros = estimated_duration
+        chakra_node.attr.append(ChakraAttr(name="num_ops", int64_val=flops))
+        chakra_node.attr.append(ChakraAttr(name="tensor_size", uint64_val=tensor_size))
+        chakra_node.duration_micros = duration
+
         return chakra_node
 
     def record_fx_node(self, fx_node: fx.Node):
@@ -322,7 +342,7 @@ class ChakraConverter:
                 upstream_search_queue.append(next_upstream)
         return
 
-    def process_fx_node(self, fx_node: fx.Node):
+    def process_fx_node(self, fx_node: fx.node, csv_writer=None):
         # Record node info in internal lookup tables.
         self.record_fx_node(fx_node)
 
@@ -345,21 +365,37 @@ class ChakraConverter:
 
         # Convert to Chakra node.
         # Check if it should be converted to a COMP or a COMM node.
+        chakra_node = None
         if is_comm_node(fx_node):
             chakra_node = self.create_comm_node(fx_node)
         elif is_comp_node(fx_node):
+            _, _, a_shape, b_shape, duration = self._profile_comp_node(fx_node)
+
             chakra_node = self.create_comp_node(fx_node)
+
+            if os.environ.get("RANK") == "0" and duration != 0 and csv_writer:
+                a_shape_str = str(tuple(a_shape))
+                b_shape_str = str(tuple(b_shape))
+                csv_writer.writerow([fx_node.name, a_shape_str, b_shape_str, duration])
         else:
-            print(f"{node_debug_id_str(fx_node)} is neither c10d or aten")
+            print(f"Node '{fx_node.name}' is neither a communication nor a computation node. Skipping.")
             return
 
         # Add the upstream dependency to the created chakra node.
-        self.add_upstream_dependency(chakra_node, fx_node)
-        self.add_to_chakra_graph(chakra_node, fx_node)
+        if chakra_node:
+            self.add_upstream_dependency(chakra_node, fx_node)
+            self.add_to_chakra_graph(chakra_node, fx_node)
 
     def convert_to_chakra(self, gm: fx.GraphModule):
         with open(self.filename, "wb") as et:
             self.et_file = et
             encode_message(et, GlobalMetadata(version="0.0.4"))
-            for fx_node in gm.graph.nodes:
-                self.process_fx_node(fx_node)
+            csv_path = "/workspace/chakra_fx/gemm_collected.csv"
+            file_previously_existed = os.path.exists(csv_path)
+            with open(csv_path, "a", newline="") as csv_file:
+                writer = csv.writer(csv_file)
+                if not file_previously_existed:
+                    writer.writerow(["node_name", "a_shape", "b_shape", "duration"])
+                for fx_node in gm.graph.nodes:
+                    self.process_fx_node(fx_node, writer)
+
