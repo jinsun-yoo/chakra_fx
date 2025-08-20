ed295bd13cabd0286111ce0d46bccb5717d9b888
Changhai Man <changhai0109@hotmail.com>
2025-08-20 00:21:05 -0400

    add generate slurm for more setups

--- launch_jobx/h100/generate_slurms.py
+++ launch_jobx/h100/generate_slurms.py
@@ -39,23 +39,10 @@ srun --ntasks=$SLURM_NNODES --ntasks-per-node=1 \
     --job eager_kineto \
     --model %s \
     --dse_config_filepath %s
-
-export CUDA_VISIBLE_DEVICES=0
-srun --nodes=1 --ntasks=1 --ntasks-per-node=1 \
-  torchrun \
-  --nnodes=1 \
-  --nproc_per_node=%d \
-  --rdzv_backend=c10d \
-  --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
-  --rdzv_id=$SLURM_JOB_ID \
-  profile_fxgraph.py \
-    --fxgraph_actions chakra \
-    --exp_tag %s \
-    --job fwbw \
-    --model %s \
-    --dse_config_filepath %s
     
 """
+
+fx_graph_template = "torchrun --nnodes=1 --nproc_per_node=%d profile_fxgraph.py --fxgraph_actions chakra --exp_tag %s --job fwbw --model %s --dse_config_filepath %s"
 
 
 def generate_slurm(job_name, model, dse_config, num_gpus, num_nodes, gpu_type):
@@ -75,72 +62,113 @@ def generate_slurm(job_name, model, dse_config, num_gpus, num_nodes, gpu_type):
     )
     return slurm_script
 
+def generate_fx_graph(job_name, model, dse_config, num_gpus, _, _):
+    return fx_graph_template % (num_gpus, job_name, model, dse_config)
+
 
 # fmt: off
 configs = [
-    # ("dp4_llama_small", "llama_small", "configs/llama_dp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("dp8_llama_small", "llama_small", "configs/llama_dp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp8_llama_small", "llama_small", "configs/llama_dp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
     ("dp16_llama_small", "llama_small", "configs/llama_dp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("dp32_llama_small", "llama_small", "configs/llama_dp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("dp64_llama_small", "llama_small", "configs/llama_dp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
-    # ("fsdp4_llama_small", "llama_small", "configs/llama_fsdp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("fsdp8_llama_small", "llama_small", "configs/llama_fsdp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp32_llama_small", "llama_small", "configs/llama_dp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("dp64_llama_small", "llama_small", "configs/llama_dp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("fsdp8_llama_small", "llama_small", "configs/llama_fsdp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
     ("fsdp16_llama_small", "llama_small", "configs/llama_fsdp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("fsdp32_llama_small", "llama_small", "configs/llama_fsdp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("fsdp64_llama_small", "llama_small", "configs/llama_fsdp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
-    # ("tp4_llama_small", "llama_small", "configs/llama_tp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("tp8_llama_small", "llama_small", "configs/llama_tp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    ("tp16_llama_small", "llama_small", "configs/llama_tp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("tp32_llama_small", "llama_small", "configs/llama_tp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("tp64_llama_small", "llama_small", "configs/llama_tp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    ("fsdp32_llama_small", "llama_small", "configs/llama_fsdp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("fsdp64_llama_small", "llama_small", "configs/llama_fsdp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
     
-    # # ("dp4_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("dp8_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("dp16_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("dp32_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("dp64_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
-    # # ("fsdp4_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("fsdp8_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("fsdp16_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("fsdp32_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("fsdp64_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
-    # # ("tp4_llama_tiny", "llama_tiny", "configs/llama_tp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("tp8_llama_tiny", "llama_tiny", "configs/llama_tp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("tp16_llama_tiny", "llama_tiny", "configs/llama_tp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("tp32_llama_tiny", "llama_tiny", "configs/llama_tp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("tp64_llama_tiny", "llama_tiny", "configs/llama_tp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    ("tpcp8_8_llama_small", "llama_small", "configs/llama_tp_cp8/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("tpcp8_16_llama_small", "llama_small", "configs/llama_tp_cp8/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("tpcp8_32_llama_small", "llama_small", "configs/llama_tp_cp8/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("tpcp8_64_llama_small", "llama_small", "configs/llama_tp_cp8/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("dp2fsdp2cp2tp_8_llama_small", "llama_small", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_16_llama_small", "llama_small", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_32_llama_small", "llama_small", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_64_llama_small", "llama_small", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("tp8cp_8_llama_small", "llama_small", "configs/llama_tp8_cp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("tp8cp_16_llama_small", "llama_small", "configs/llama_tp8_cp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("tp8cp_32_llama_small", "llama_small", "configs/llama_tp8_cp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("tp8cp_64_llama_small", "llama_small", "configs/llama_tp8_cp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
 
-    # # ("dp4_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("dp8_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("dp16_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("dp32_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("dp64_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
-    # # ("fsdp4_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("fsdp8_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("fsdp16_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("fsdp32_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("fsdp64_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
-    # # ("tp4_llama_3b", "llama_3b", "configs/llama_tp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("tp8_llama_3b", "llama_3b", "configs/llama_tp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("tp16_llama_3b", "llama_3b", "configs/llama_tp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("tp32_llama_3b", "llama_3b", "configs/llama_tp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("tp64_llama_3b", "llama_3b", "configs/llama_tp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+
+    ("dp8_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp16_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("dp32_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("dp64_llama_tiny", "llama_tiny", "configs/llama_dp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
     
-    # # ("dp4_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("dp8_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("dp16_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("dp32_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("dp64_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
-    # # ("fsdp4_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("fsdp8_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("fsdp16_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("fsdp32_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("fsdp64_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
-    # # ("tp4_llama_1b", "llama_1b", "configs/llama_tp/llama_FSDP_4.yml", 4, 1, "gpu:H100:8",),
-    # ("tp8_llama_1b", "llama_1b", "configs/llama_tp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
-    # ("tp16_llama_1b", "llama_1b", "configs/llama_tp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
-    # ("tp32_llama_1b", "llama_1b", "configs/llama_tp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
-    # ("tp64_llama_1b", "llama_1b", "configs/llama_tp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    ("fsdp8_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("fsdp16_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("fsdp32_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("fsdp64_llama_tiny", "llama_tiny", "configs/llama_fsdp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("tpcp8_8_llama_tiny", "llama_tiny", "configs/llama_tp_cp8/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("tpcp8_16_llama_tiny", "llama_tiny", "configs/llama_tp_cp8/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("tpcp8_32_llama_tiny", "llama_tiny", "configs/llama_tp_cp8/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("tpcp8_64_llama_tiny", "llama_tiny", "configs/llama_tp_cp8/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("dp2fsdp2cp2tp_8_llama_tiny", "llama_tiny", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_16_llama_tiny", "llama_tiny", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_32_llama_tiny", "llama_tiny", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_64_llama_tiny", "llama_tiny", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("tp8cp_8_llama_tiny", "llama_tiny", "configs/llama_tp8_cp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("tp8cp_16_llama_tiny", "llama_tiny", "configs/llama_tp8_cp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("tp8cp_32_llama_tiny", "llama_tiny", "configs/llama_tp8_cp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("tp8cp_64_llama_tiny", "llama_tiny", "configs/llama_tp8_cp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    
+    ("dp8_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp16_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("dp32_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("dp64_llama_1b", "llama_1b", "configs/llama_dp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("fsdp8_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("fsdp16_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("fsdp32_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("fsdp64_llama_1b", "llama_1b", "configs/llama_fsdp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("tpcp8_8_llama_1b", "llama_1b", "configs/llama_tp_cp8/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("tpcp8_16_llama_1b", "llama_1b", "configs/llama_tp_cp8/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("tpcp8_32_llama_1b", "llama_1b", "configs/llama_tp_cp8/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("tpcp8_64_llama_1b", "llama_1b", "configs/llama_tp_cp8/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("dp2fsdp2cp2tp_8_llama_1b", "llama_1b", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_16_llama_1b", "llama_1b", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_32_llama_1b", "llama_1b", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_64_llama_1b", "llama_1b", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("tp8cp_8_llama_1b", "llama_1b", "configs/llama_tp8_cp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("tp8cp_16_llama_1b", "llama_1b", "configs/llama_tp8_cp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("tp8cp_32_llama_1b", "llama_1b", "configs/llama_tp8_cp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("tp8cp_64_llama_1b", "llama_1b", "configs/llama_tp8_cp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("dp8_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp16_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("dp32_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("dp64_llama_3b", "llama_3b", "configs/llama_dp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("fsdp8_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("fsdp16_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("fsdp32_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("fsdp64_llama_3b", "llama_3b", "configs/llama_fsdp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("tpcp8_8_llama_3b", "llama_3b", "configs/llama_tp_cp8/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("tpcp8_16_llama_3b", "llama_3b", "configs/llama_tp_cp8/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("tpcp8_32_llama_3b", "llama_3b", "configs/llama_tp_cp8/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("tpcp8_64_llama_3b", "llama_3b", "configs/llama_tp_cp8/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("dp2fsdp2cp2tp_8_llama_3b", "llama_3b", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_16_llama_3b", "llama_3b", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_32_llama_3b", "llama_3b", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("dp2fsdp2cp2tp_64_llama_3b", "llama_3b", "configs/llama_dp2_fsdp2_cp2_tp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
+    
+    ("tp8cp_8_llama_3b", "llama_3b", "configs/llama_tp8_cp/llama_FSDP_8.yml", 8, 1, "gpu:H100:8",),
+    ("tp8cp_16_llama_3b", "llama_3b", "configs/llama_tp8_cp/llama_FSDP_16.yml", 16, 2, "gpu:H100:8",),
+    ("tp8cp_32_llama_3b", "llama_3b", "configs/llama_tp8_cp/llama_FSDP_32.yml", 32, 4, "gpu:H100:8",),
+    ("tp8cp_64_llama_3b", "llama_3b", "configs/llama_tp8_cp/llama_FSDP_64.yml", 64, 8, "gpu:H100:8",),
 ]
 # fmt: on
 
@@ -150,3 +178,9 @@ for dp in configs:
         slurm_script = generate_slurm(*dp)
         f.write(slurm_script)
         print(f"Generated {job_name}.slurm")
+
+for dp in configs:
+    with open("torchfx.sh", "w") as f:
+        fx_graph_script = generate_fx_graph(*dp)
+        print(fx_graph_script, file=f)
+        print(f"Generated torchfx.sh for {dp[0]}")
