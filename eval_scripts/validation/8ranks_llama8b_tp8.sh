6d3222ebe4af76248cde002d7316ec69b1821ad9
Changhai Man <changhai0109@hotmail.com>
2025-10-28 16:16:11 -0400

    add validation scripts llama 1B, 3B, debug

--- eval_scripts/validation/8ranks_llama8b_tp8.sh
+++
@@ -1,8 +0,0 @@
-torchrun \
-    --nproc-per-node=8 \
-    ../../profile_fxgraph.py \
-    --exp_tag llama8b_tp8_8ranks \
-    --job postexec_chakra \
-    --model llama \
-    --dse_config_filepath configs/llama_TP8.yml \
-    --llama_config 8B
