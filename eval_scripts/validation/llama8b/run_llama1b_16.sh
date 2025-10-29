66382fb6a546e35cd71f282d3922975d4eeb6b52
AlexandreGUO2001 <weiguo200101@gmail.com>
2025-10-29 02:01:50 -0400

    add tp2 for no oom

--- eval_scripts/validation/llama8b/run_llama1b_16.sh
+++ eval_scripts/validation/llama8b/run_llama1b_16.sh
@@ -1,7 +1,11 @@
-bash ./eval_scripts/validation/llama8b/16ranks_llama1B_dp16.sh 
-bash ./eval_scripts/validation/llama8b/16ranks_llama1B_fsdp16.sh 
-bash ./eval_scripts/validation/llama8b/16ranks_llama1B_fsdp2_dp8.sh 
-bash ./eval_scripts/validation/llama8b/16ranks_llama1B_fsdp4_dp4.sh 
+# bash ./eval_scripts/validation/llama8b/16ranks_llama1B_dp16.sh 
+# bash ./eval_scripts/validation/llama8b/16ranks_llama1B_fsdp16.sh 
+# bash ./eval_scripts/validation/llama8b/16ranks_llama1B_fsdp2_dp8.sh 
+# bash ./eval_scripts/validation/llama8b/16ranks_llama1B_fsdp4_dp4.sh 
+bash ./eval_scripts/validation/llama8b/16ranks_llama1B_tp2_dp8.sh 
+bash ./eval_scripts/validation/llama8b/16ranks_llama1B_tp2_fsdp8.sh 
+bash ./eval_scripts/validation/llama8b/16ranks_llama1B_tp2_fsdp2_dp4.sh 
+bash ./eval_scripts/validation/llama8b/16ranks_llama1B_tp2_fsdp4_dp2.sh 
 bash ./eval_scripts/validation/llama8b/16ranks_llama1B_tp16.sh 
 bash ./eval_scripts/validation/llama8b/16ranks_llama1B_tp4_fsdp2_dp2.sh 
 bash ./eval_scripts/validation/llama8b/16ranks_llama1B_tp4_fsdp4.sh 
